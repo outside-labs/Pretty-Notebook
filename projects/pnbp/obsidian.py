319ec389f59ffe3b2eae896b10d2160aec063f75
ellainng <299958882+ellainng@users.noreply.github.com>
2025-10-05 18:30:57 -0700

    feat(notebook): resolve links across notebook notes
    Build notebook-level parsing around related Markdown notes and their internal links.

--- projects/pnbp/obsidian.py
+++ projects/pnbp/obsidian.py
@@ -1,51 +1,23 @@
 import os
 import subprocess
 import json
-
 import click
 
 from obsidianotebook import ObsidianNotebook
+
 
 @click.group()
 def obsidiancli():
 	pass
 
 
-@click.command()
-@click.option('--path', default='.', help='File path to new project')
-def create_subl_proj(path):
-	""" Command to initiate local Sublime Text project && symlink to main Obsisian Notes directory"""
-	defaultJson = {
-		"folders":
-		[
-			{
-				"path": ".",
-				"folder_exclude_patterns": ["__pycache__"],
-			}
-		],
-		"settings": {
-			"python_virtualenv": "/Users/curtis/prog/.envs/venv/",
-			"python_interpreter": "/Users/curtis/prog/.envs/venv/bin/python",
-			"python_package_paths": [
-			]
-		}
-	}
 
-	if path == '.':
-		path = os.getcwd()
-
-	projname = path.split('/')[-1]
-
-	with open(os.path.join(path, f'{projname}.sublime-project'), 'w') as f:
-		f.write(json.dumps(defaultJson, indent=4))
-
-	x = subprocess.run(['ln', '-s', os.path.join(path, f'{projname}.sublime-project'), ObsidianNotebook.NOTE_PATH], capture_output=True)
-	click.echo(x)
-
-
+""" writing collections to specific notebook files:
+"""
 @click.command()
 def collect_subl_projects():
-	""" -> notebook/sublime-project.md"""
+	""" notebook/example.sublime_project -> sublime-project.md
+	"""
 	projs = []
 	for fn in os.listdir(ObsidianNotebook.NOTE_PATH):
 		if fn.endswith('.sublime-project'):
@@ -57,7 +29,47 @@ def collect_subl_projects():
 		f.write(obsidianmd)
 
 
+@click.command()
+def collect_all_notes():
+	""" -> notebook/all notes.md"""
+	ns = []
+	for fn in os.listdir(ObsidianNotebook.NOTE_PATH):
+		if fn.endswith('.md'):
+			ns.append(fn)
 
+	obsidianmd = "".join([f"[[{x.split('.')[0]}]]\n" for x in ns])
+
+	with open(os.path.join(ObsidianNotebook.NOTE_PATH, 'all notes.md'), 'w') as f:
+		f.write(obsidianmd)
+
+
+@click.command()
+def collect_all_urls():
+	""" -> notebook/all urls.md"""
+	nb = ObsidianNotebook()
+
+	all_urls = []
+	for n in nb.notes.values():
+		if n.name not in ('all urls'):
+			for u in n.urls:
+				all_urls.append(u)
+
+	with open(os.path.join(nb.NOTE_PATH, 'all urls.md'), 'w') as f:
+		f.write('\n'.join(str(l) for l in all_urls))
+
+
+@click.command()
+def collect():
+	""" do all above collect- commands in succession """
+	collect_subl_projects()
+	collect_all_notes()
+	collect_all_urls()
+
+
+
+
+""" obsidian-blog api connection:
+"""
 @click.command()
 def commit_local_html():
 	""" if note contains #public, -> blog """
@@ -80,8 +92,10 @@ def commit_local_api():
 obsidiancli.add_command(commit_local_html)
 obsidiancli.add_command(commit_local_api)
 
-obsidiancli.add_command(create_subl_proj)
 obsidiancli.add_command(collect_subl_projects)
+obsidiancli.add_command(collect_all_notes)
+obsidiancli.add_command(collect_all_urls)
+obsidiancli.add_command(collect)
 
 if __name__ == '__main__':
 	obsidiancli()
--- projects/pnbp/obsidianotebook.py
+++ projects/pnbp/obsidianotebook.py
@@ -1,20 +1,21 @@
 import os
 import re
 import subprocess
-import markdown as md
 import json
 from collections import namedtuple, defaultdict
 
-import requests 
+import markdown as md
+import requests
+
 
 
 def int_link_repl(matchobj):
-	""" regex replacement method for [[]] internal Obsidian links -> mysite.com/single-slug
+	""" regex replacement function for [[]] internal Obsidian links -> mysite.com/single-slug
 	"""
-	return f"<a href='/{matchobj.group(1).replace('_', '-').lower()}'>{matchobj.group(1)}</a>"
+	return f"<a href='/{matchobj.group(1).replace('_', '-').replace(' ', '-').lower()}'>{matchobj.group(1)}</a>"
 
 def int_img_repl(matchobj):
-	""" regex replacement method for ![[]] internal Obsidian links -> mysite.com/single-slug
+	""" regex replacement function for ![[]] internal Obsidian links -> mysite.com/single-slug
 		todo: accept clean ( .png | .jpg ||)
 	"""
 	return f"""<img class="img-fluid" src='static/imgs/{matchobj.group(1)}'>"""
@@ -23,86 +24,160 @@ def int_tag_repl(matchobj):
 	""" regex #tags out to distinguish vs # space means md header1 """
 	return f"\\#{matchobj.group(1)}"
 
+def md_mermaid_repl(matchobj):
+	""" re """
+	return f'<div class="mermaid">{matchobj.group(1)}</div>'
 
-ObsidianNote = namedtuple('Note', ['name', 'of'])
+def md_nakedhref_repl(matchobj):
+	""" regex replacement function for e.g. http://www.blahblahblah.com -> 
+		[http://www.blahblahblah.com](http://www.blahblahblah.com)
+		markdown syntax so that on md -> html, these links are clickable
+	"""
+	return f"[{matchobj.group(1)}]({matchobj.group(1)})"
+
+
+
+ObsidianNote = namedtuple('Note', ['name', 'md', 'links', 'tags', 'urls'])
+
 
 
 class ObsidianNotebook:
 
 	OBS_INT_LNK = r'\[\[([^]]+)\]\]'
 	OBS_IMG_LNK = r'!\[\[([^]]+)\]\]'
-	OBS_INT_TAG = r'#([A-Za-z])' 
+	OBS_INT_TAG = r'#([A-Za-z]+)' 
 
-	NOTE_PATH = '/Users/curtis/Library/Mobile Documents/com~apple~CloudDocs/Stuff/onotes'
-	IMG_PATH = f'{NOTE_PATH}/imgs'
+	MD_CODE = r'```([^`]*)```' #.\s\S
+	MD_MERMAID = r'```mermaid([^`]*)```'
 
-	HTML_PATH = '/Users/curtis/oblog'
-	# -> services/
+	MD_EXT_LINK = r'\[([^]]+)\]\(([^)]+)\)'
+	HTTP_NAKED_LNK = r'[^\[](https?://[^;,\s\]\*]+)'
 
 	COMMIT_TAG = '#public'
 
-
 	def __init__(self):
-		self.notes = defaultdict()
-		self.open()
+		if not os.path.exists('settings.json'):
+			raise ImportError("required settings.json file not found, please see settings_template.json")
 		
+		with open('settings.json') as cf:
+			self.config = json.load(cf)
 
-	def open(self):
+		self.NOTE_PATH = self.config.get('NOTE_PATH')
+		self.IMG_PATH = self.config.get('IMG_PATH')
+		self.HTML_PATH = self.config.get('HTML_PATH')
+
+		self.notes = defaultdict()
+		self.open_md()
+
+	def open_md(self):
 		""" open all files in the Obsidian Notebook path into memory """
 		for f in os.listdir(self.NOTE_PATH):
 			fname = f.split('.')[0]
 			if f.endswith('.md'):
 				with open(os.path.join(self.NOTE_PATH, f), 'r') as fo:
-					n = ObsidianNote(name=fname, of=fo.read())
+					fo = fo.read()
+
+					n = ObsidianNote(
+						name=fname,
+						md=fo,
+						links=re.findall(self.OBS_INT_LNK, fo),
+						tags=re.findall(self.OBS_INT_TAG, fo),
+						urls=self.get_urls(fo)
+						)
+
 					self.notes.update({fname: n})
 
+	def find(self, regex):
+		for f, n in self.notes.items():
+			p = re.compile(regex)
 
+	def get_urls(self, note):
+		ext_links = []
 
+		p = re.compile(self.MD_EXT_LINK)
+		for l in p.findall(note):
+			ext_links.append(l[1])
+
+		p = re.compile(self.HTTP_NAKED_LNK)
+		for l in p.findall(note):
+			ext_links.append(l.rstrip('.').rstrip(')'))
+
+		return list(set(ext_links))
+	
+	def replace_imglinks(self, note):
+		p = re.compile(self.OBS_IMG_LNK)
+		return p.sub(int_img_repl, note)
+
+	def replace_obslinks(self, note):
+		p = re.compile(self.OBS_INT_LNK)
+		return p.sub(int_link_repl, note)
+
+	def replace_obstags(self, note):
+		p = re.compile(self.OBS_INT_TAG)
+		return p.sub(int_tag_repl, note)
+
+	def replace_mermaid(self, note):
+		p = re.compile(self.MD_MERMAID)
+		return p.sub(md_mermaid_repl, note)
+
+	def replace_nakedhref(self, note):
+		p = re.compile(self.HTTP_NAKED_LNK)
+		return p.sub(md_nakedhref_repl, note)
 
 	def convert_to_html(self, note):
 		""" a single note """
 
-		p = re.compile(self.OBS_IMG_LNK)
-		nout = p.sub(int_img_repl, note)
-		# print(nout)
+		nout = self.replace_imglinks(note)
+		nout = self.replace_obslinks(nout)
+		nout = self.replace_obstags(nout)
+		nout = self.replace_mermaid(nout)
+		nout = self.replace_nakedhref(nout)
 
-		p = re.compile(self.OBS_INT_LNK)
-		nout = p.sub(int_link_repl, nout)
-
-		p = re.compile(self.OBS_INT_TAG)
-		nout = p.sub(int_tag_repl, nout)
-
-		nout = md.markdown(nout, extensions=['fenced_code'])
+		nout = md.markdown(nout, extensions=['fenced_code', 'nl2br'])
 
 		return nout
 
 
 	def write_commits_to_local_html(self):
 		"""  """
-		self.open()
+		self.open_md() # fresh retrival 
+
+		print(f'\nlocal commit: {self.HTML_PATH}')
 		for n in self.notes.values():
-			if re.search(self.COMMIT_TAG, n.of):
-				nout = self.convert_to_html(note=n.of)
-				of = open(os.path.join(self.HTML_PATH, f'{n.name}.html'), 'w')
+			if re.search(self.COMMIT_TAG, n.md):
+				nout = self.convert_to_html(note=n.md)
+				of = open(os.path.join(self.HTML_PATH, f"{n.name.replace('_', '-').replace(' ', '-').lower()}.html"), 'w')
 				of.write(nout) #https://python-markdown.github.io/extensions/fenced_code_blocks/
 				of.close()
 
-				print(f'{n.name} ---> {self.HTML_PATH}')
+				print(f'\t{n.name} ---> {self.HTML_PATH}')
 
 
 	def post_commits_to_blog_api(self):
 		""" """
-		self.open()
-		for n in self.notes.values():
-			if re.search(self.COMMIT_TAG, n.of):
-				nout = self.convert_to_html(note=n.of)
-				r = requests.post('http://127.0.0.1:8000/api/publishment', json={"name": n.name, "content": nout})
-				print(r)
+		self.open_md() # fresh retrival
 
-				for img in re.findall(self.OBS_IMG_LNK, n.of):
+		print(f'\nremote commit:')
+		for n in self.notes.values():
+			if re.search(self.COMMIT_TAG, n.md):
+				nout = self.convert_to_html(note=n.md)
+				r = requests.post('http://127.0.0.1:8000/api/publishment',
+					json={"name": n.name.replace('_', '-').replace(' ', '-').lower(), "content": nout})
+				
+				print(f'\t{n.name} -> {r}')
+
+				for img in re.findall(self.OBS_IMG_LNK, n.md):
 					f = open(os.path.join(self.IMG_PATH, img), 'rb')
-					r = requests.post('http://127.0.0.1:8000/api/image', files={"filename": img, "file": f, "content_type": "image/jpeg"})
-					print(r.content)
+					r = requests.post('http://127.0.0.1:8000/api/image',
+						files={"filename": img, "file": f, "content_type": "image/jpeg"})
+					
+					print(f'\t{img} -> {r}') # r.content = {"filename": "examp.jpeg"}
+
+
+	def get_pub_commits(self):
+		""" """
+		r = requests.get('http://127.0.0.1:8000/api/publishments')
+		print(r.json())
 
 
 
@@ -119,17 +194,6 @@ def main():
 
 if __name__ == '__main__':
 	main()
-	
 
 
-
-
-
-
-
-# print(subprocess.run(['git', 'status'], capture_output=True))
-# if subprocess.run(['git', 'status'], capture_output=True).stderr == b'fatal: not a git repository (or any of the parent directories): .git\n':
-# 	subprocess.run(['git', 'init'], capture_output=True)
-
-# b'On branch master\n\nNo commits yet\n\nUntracked files:\n  (use "git add <file>..." to include in what will be committed)\n\tnoncommit.md\n\tobsidianparse.py\n\ttest.html\n\ttest.md\n\nnothing added to commit but untracked files present (use "git add" to track)\n'
 
